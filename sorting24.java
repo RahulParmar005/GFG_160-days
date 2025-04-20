3706ec2fa18a166b2576d3b0b450a8347d687217
RahulParmar005 <rahulparmarrr0005@gmail.com>
2025-04-20 17:08:03 +0530

    Add the solution of day 24

---
+++ sorting24.java
@@ -0,0 +1,24 @@
+// Given an array of Intervals arr[][], where arr[i] = [starti, endi]. The task is to merge all of the overlapping Intervals.
+
+import java.util.*;
+
+class Solution {
+    public List<int[]> mergeOverlap(int[][] arr) {
+        // Code here // Code here
+        Arrays.sort(arr, (a,b) -> Integer.compare(a[0], b[0]));
+        List<int[]> res = new ArrayList<>();
+        res.add(new int[] {arr[0][0], arr[0][1]});
+        
+        for(int i=1; i<arr.length; i++){
+            int[] last = res.get(res.size() - 1);
+            int[] curr = arr[i];
+            
+            if(curr[0] <= last[1]){
+                last[1] = Math.max(last[1], curr[1]);;
+            } else {
+                res.add(new int[] {curr[0], curr[1]});
+            }
+        }
+        return res;
+    }
+}
