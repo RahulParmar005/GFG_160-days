b2ab6aadc5bbc492138048731192fe7a8999c7c3
RahulParmar005 <rahulparmarrr0005@gmail.com>
2025-05-19 16:45:24 +0530

    Add the solution of day 52

---
+++ twoPointers52.java
@@ -0,0 +1,23 @@
+// Given an array arr[] and an integer target. You have to find the number of pairs in the array whose sum is strictly less than the target.
+import java.util.*;
+
+class Solution {
+    int countPairs(int arr[], int target) {
+        // Your code here
+        Arrays.sort(arr);
+        int left = 0, right = arr.length - 1;
+        int  count = 0;
+        
+        while(left < right){
+            int sum = arr[left] + arr[right];
+            
+            if(sum < target){
+                count += right - left;
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return count;
+    }
+}
